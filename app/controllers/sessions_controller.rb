fbf960a78143c41860335ebdeeea7e3c67fb2358
github username <github email address>
2019-10-24 17:14:00 -0500

    sup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,8 @@ end
 
 
 def create 
-  @user = User.find_by(username:params[:username])
+  byebug
+  @user = User.find_by(username: params[:username])
   session[:user_id] = @user.id
 
   if @user.authenticate(params[:password])
